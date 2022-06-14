981ea62833b57c72dfc8cf0a31c94ce87898ba75
maoqiang02 <maoqiang02@meituan.com>
2022-06-14 10:44:26 +0800

    add cr case

---
+++ src/test/java/org/springframework/cloud/gateway/util/Numbers.java
@@ -0,0 +1,11 @@
+package org.springframework.cloud.gateway.util;
+
+public final class Numbers {
+
+    /**
+     * 判断一个整型是否是奇数
+     */
+    public boolean isOdd(int number) {
+        return number % 2 == 1;
+    }
+}
