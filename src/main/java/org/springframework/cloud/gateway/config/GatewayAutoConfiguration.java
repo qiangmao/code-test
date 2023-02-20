c39e9e17855a50a8ce302577277d68e4d7f1129b
Qiang Mao <maoqiang321@gmail.com>
2023-02-20 20:20:11 +0800

    Update GatewayAutoConfiguration.java

--- src/main/java/org/springframework/cloud/gateway/config/GatewayAutoConfiguration.java
+++ src/main/java/org/springframework/cloud/gateway/config/GatewayAutoConfiguration.java
@@ -41,6 +41,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.NoneNestedConditions;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
